406614596f494768f57d62eb79e66693e1c62f8d
David Gao <dsgao@andrew.cmu.edu>
2016-01-20 03:32:42 -0800

    Changed schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120105609) do
+ActiveRecord::Schema.define(version: 20160120112942) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
-    t.text     "contet"
     t.integer  "category_id"
     t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "content"
   end
 
   create_table "categories", force: true do |t|
